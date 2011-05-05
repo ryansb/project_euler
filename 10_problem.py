604548f7d3744191863cfd56558008241f9b9caf
Ryan Brown <ryansb@csh.rit.edu>
2011-05-05 15:18:26 -0400

    Solved problem 10

---
+++ 10_problem.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# Author: Ryan Brown
+# Description: Find the sum of all the primes below two million.
+
+def gen_primes():
+    """ Generate an infinite sequence of prime numbers.
+    """
+    # Maps composites to primes witnessing their compositeness.
+    # This is memory efficient, as the sieve is not "run forward"
+    # indefinitely, but only as long as required by the current
+    # number being tested.
+    #
+    D = {}
+
+    # The running integer that's checked for primeness
+    q = 2
+
+    while True:
+        if q not in D:
+            # q is a new prime.
+            # Yield it and mark its first multiple that isn't
+            # already marked in previous iterations
+            yield q
+            D[q * q] = [q]
+        else:
+            # q is composite. D[q] is the list of primes that
+            # divide it. Since we've reached q, we no longer
+            # need it in the map, but we'll mark the next
+            # multiples of its witnesses to prepare for larger
+            # numbers
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+
+        q += 1
+
+np = 0
+prim = []
+p = gen_primes()
+while np < 2000000:
+	prim.append(np)
+	np = p.next()
+print sum(prim)
