b3a3ccebc31f4a38b570344fdcd08259cfb3c945
Ryan Brown <ryansb@csh.rit.edu>
2011-05-04 23:10:42 -0400

    Solved my first problem

---
+++ 1_problem.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Author: Ryan Brown <rbrown@newstex.com>
+# Description: 
+nat_nums = set()
+for i in range(0,1000):
+	if(i % 3 == 0):
+		nat_nums.add(i)
+	if(i % 5 == 0):
+		nat_nums.add(i)
+print sum(nat_nums)
